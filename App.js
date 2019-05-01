6f353f7d62b894aa7a0d21c745a8f1d39731b39e
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-01 10:52:02 -0500

    connected app to firebase

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import firebase from 'firebase'
 import reducers from './reducers'
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDL4znPhFnoLiKg7sOT8ls7yDFRnV1f4ME",
+      authDomain: "employee-manager-8faef.firebaseapp.com",
+      databaseURL: "https://employee-manager-8faef.firebaseio.com",
+      projectId: "employee-manager-8faef",
+      storageBucket: "employee-manager-8faef.appspot.com",
+      messagingSenderId: "26772640181"
+    }
+    firebase.initializeApp(config)
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
